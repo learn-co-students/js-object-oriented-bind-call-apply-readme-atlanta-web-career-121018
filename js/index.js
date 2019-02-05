ac0d8c290573486213756689a1820f4b46f61ed7
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-05 08:05:39 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+  
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+ 
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' }; 
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+} 
